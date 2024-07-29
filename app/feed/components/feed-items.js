a927db7be19ac995fbaa807fad5fd05d623ce366
Adam Huffman <coronablue3@gmail.com>
2024-07-29 16:30:28 -0500

    Move feed aggregation into separate function.

--- app/feed/components/feed-items.js
+++ app/feed/components/feed-items.js
@@ -1,6 +1,22 @@
 import { pool } from "@/app/data/db-manager";
 import ItemCard from "./item-card";
 
+function aggregateFeed(feeds) {
+    let aggFeed = [];
+
+    feeds.map((feed) => {
+        feed.items.map((item) => {
+            aggFeed.push({
+                feedTitle: feed.title,
+                feedFavicon: feed.favicon,
+                ...item
+            });
+        });
+    });
+
+    return aggFeed;
+}
+
 export default async function FeedItems() {
     // Get feeds from database
     const client = await pool.connect();
@@ -8,17 +24,7 @@ export default async function FeedItems() {
     await client.end();
     let feeds = result.rows;
 
-    //Aggregate feeds
-    let agFeeds = [];
-    feeds.map((feed) => {
-        feed.items.map((item) => {
-            agFeeds.push({
-                feedTitle: feed.title,
-                feedFavicon: feed.favicon,
-                ...item
-            });
-        });
-    });
+    let agFeeds = aggregateFeed(feeds);
 
     return (
         <div class="col-span-4 bg-teal-400">
