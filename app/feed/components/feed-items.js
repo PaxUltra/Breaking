e1fe3e3d5df5d55b1205bb51ab4a9d00be6b46a5
Adam Huffman <coronablue3@gmail.com>
2024-07-30 15:53:10 -0500

    Add ability to sort feed based on limited set of date ranges.

--- app/feed/components/feed-items.js
+++ app/feed/components/feed-items.js
@@ -1,5 +1,9 @@
+import dayjs from "dayjs";
+import isYesterday from 'dayjs/plugin/isYesterday';
 import { pool } from "@/app/data/db-manager";
 import ItemCard from "./item-card";
+
+dayjs.extend(isYesterday);
 
 function aggregateFeed(feeds) {
     let aggFeed = [];
@@ -18,13 +22,55 @@ function aggregateFeed(feeds) {
 }
 
 function sortFeed(agFeed) {
+    let sortedFeed = {};
     let sortedByDate = agFeed.toSorted((a, b) => {
         return new Date(b.pubDate) - new Date(a.pubDate);
     });
 
-    let sortedFeed = {};
+    sortedByDate.map((item) => {
+        let pubDate = dayjs(item.pubDate);
 
-    return sortedByDate;
+        if (dayjs().isSame(pubDate, 'day')) {
+            if (!('Today' in sortedFeed)) {
+                sortedFeed['Today'] = [];
+            }
+
+            sortedFeed['Today'].push(item);
+        } else if (pubDate.isYesterday()) {
+            if (!('Yesterday' in sortedFeed)) {
+                sortedFeed['Yesterday'] = [];
+            }
+
+            sortedFeed['Yesterday'].push(item);
+        } else if (dayjs().isSame(pubDate, 'week')) {
+            if (!('This Week' in sortedFeed)) {
+                sortedFeed['This Week'] = [];
+            }
+
+            sortedFeed['This Week'].push(item);
+        } else if (dayjs().isSame(pubDate, 'month')) {
+            if (!('This Month' in sortedFeed)) {
+                sortedFeed['This Month'] = [];
+            }
+
+            sortedFeed['This Month'].push(item);
+        } else if (dayjs().subtract(1, 'month').isSame(pubDate, 'month')) {
+            if (!('Last Month' in sortedFeed)) {
+                sortedFeed['Last Month'] = [];
+            }
+
+            sortedFeed['Last Month'].push(item);
+        } else {
+            let monthName = pubDate.format('MMMM');
+            if (!(monthName in sortedFeed)) {
+                sortedFeed[monthName] = [];
+            }
+
+            sortedFeed[monthName].push(item);
+        }
+    });
+
+    return sortedFeed;
 
 }
 
@@ -38,13 +84,23 @@ export default async function FeedItems() {
     let agFeeds = aggregateFeed(feeds);
     let sortedFeed = sortFeed(agFeeds);
 
+    Object.keys(sortedFeed).forEach((key) => {
+        console.log(key)
+    })
     return (
         <div class="col-span-4 bg-teal-400">
-            {sortedFeed.map((item) => (
-                <div>
-                    <ItemCard feed={item.feedTitle} favicon={item.feedFavicon} item={item} />
-                </div>
-            ))}
+            {Object.keys(sortedFeed).map((key) => {
+                return (
+                    <div>
+                        <p>{key}</p>
+                        {sortedFeed[key].map((item) => (
+                            <div>
+                                <ItemCard feed={item.feedTitle} favicon={item.feedFavicon} item={item} />
+                            </div>
+                        ))}
+                    </div>
+                );
+            })}
         </div>
     );
 }
